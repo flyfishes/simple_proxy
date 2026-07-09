1fea8edc4cbd5c84882a6dfef24cceac180f583e
flyfishes <allenzh77@gmail.com>
2026-07-09 12:05:59 +0800

    full http/https support

--- gen-cert.cmd
+++ gen-cert.cmd
@@ -1,5 +1,6 @@
 #!/bin/bash
 # 生成自签名证书
 openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 3365 -nodes \
-    -subj "/CN=localhost"
+    -subj "/CN=localhost" \
+    -addext "subjectAltName=DNS:localhost,IP:127.0.0.1,IP:0.0.0.0"
 echo "证书已生成: cert.pem, key.pem"
